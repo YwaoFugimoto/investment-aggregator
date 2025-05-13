c243459c1228755eeb4ae681abd1f45808dd07ff
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-12 23:28:19 -0300

    feat: Adding user service

--- todo/src/main/java/com/crud/todo/service/UserService.java
+++ todo/src/main/java/com/crud/todo/service/UserService.java
@@ -1,4 +1,19 @@
 package com.crud.todo.service;
 
+import com.crud.todo.controller.CreateUserDto;
+import com.crud.todo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public UserRepository userRepository;
+
+    public UserService (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void createUSer(CreateUserDto createUserDto) {
+
+    }
 }
