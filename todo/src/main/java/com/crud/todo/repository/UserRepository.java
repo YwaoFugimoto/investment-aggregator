46d931ce27c09cfc0c2241bd73129fe4fe51da2c
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-13 14:24:26 -0300

    feat: Updating user repository code

--- todo/src/main/java/com/crud/todo/repository/UserRepository.java
+++ todo/src/main/java/com/crud/todo/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.crud.todo.repository;
 
+import com.crud.todo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
