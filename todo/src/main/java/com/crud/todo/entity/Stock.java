18b40d26d69f2be35af20877c0cda5e8b5e13341
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-15 02:27:40 -0300

    feat: creating stock object

--- todo/src/main/java/com/crud/todo/entity/Stock.java
+++ todo/src/main/java/com/crud/todo/entity/Stock.java
@@ -2,29 +2,29 @@ package com.crud.todo.entity;
 
 import jakarta.persistence.*;
 
-import java.util.UUID;
+
 @Entity
 @Table(name = "tb_stocks")
 public class Stock {
 
     @Id
     @Column(name = "stock_id")
-    private String stock_id; //PETR4 M4G4L
+    private String stockId; //PETR4 M4G4L
 
     @Column(name = "description")
     private String description;
 
-    public Stock(String stock_id, String description) {
-        this.stock_id = stock_id;
+    public Stock(String stockId, String description) {
+        this.stockId = stockId;
         this.description = description;
     }
 
-    public String getStock_id() {
-        return stock_id;
+    public String getstockId() {
+        return stockId;
     }
 
-    public void setStock_id(String stock_id) {
-        this.stock_id = stock_id;
+    public void setstockId(String stockId) {
+        this.stockId = stockId;
     }
 
     public String getDescription() {
