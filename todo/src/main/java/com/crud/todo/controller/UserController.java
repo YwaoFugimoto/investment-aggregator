22bfcac54273f7d1c34c1cd523c626eb6705c119
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-14 22:51:29 -0300

    feat : List of users

--- todo/src/main/java/com/crud/todo/controller/UserController.java
+++ todo/src/main/java/com/crud/todo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -24,9 +25,20 @@ public class UserController {
         return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
     }
 
-    @GetMapping
+    @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
-        //
-        return null;
+        var user = userService.getUserById(userId);
+        if ( user.isPresent() ){
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        var users = userService.listUsers();
+        
+        return ResponseEntity.ok(users);
     }
 }
