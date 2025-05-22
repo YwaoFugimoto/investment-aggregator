aaf6d3c823cb8d7fafac2727ef60ef457b9f37ac
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-05-22 10:51:28 -0300

    feat: adding stock list

--- todo/src/main/java/com/crud/todo/service/StockService.java
+++ todo/src/main/java/com/crud/todo/service/StockService.java
@@ -1,9 +1,12 @@
 package com.crud.todo.service;
 
 import com.crud.todo.controller.dto.CreateStockDto;
+import com.crud.todo.controller.responseDto.StockResponseDto;
 import com.crud.todo.entity.Stock;
 import com.crud.todo.repository.StockRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StockService {
@@ -14,11 +17,20 @@ public class StockService {
     }
 
     public void createStock(CreateStockDto createStockDto) {
+
+        // dto -> entity
         var stock = new Stock(
                 createStockDto.stockId(),
                 createStockDto.description()
         );
 
         stockRepository.save(stock);
+    }
+
+    public List<StockResponseDto> listStocks() {
+        return stockRepository.findAll().stream().map(stock -> new StockResponseDto(
+                stock.getStockId(),
+                stock.getDescription()
+        )).toList();
     }
 }
